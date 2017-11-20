6027190f829ce5598707247b2eaf89219625278b
Chong Tang <ctang@virginia.edu>
2017-11-20 16:49:46 -0500

    change some large numbers back to real values

--- testing.v
+++ testing.v
@@ -7,20 +7,22 @@ Require Import hadoop_config.
 Environment parameter values.
 *)
 Definition total_cpu := 8.
-Definition total_mem := 4096.
+Definition total_mem := 8192.
 
 (*
 Configuration parameter values.
+
+These parameters are in mapred-site.xml
 *)
-Definition my_mapreduce_map_cpu_vcores := 4.
-Definition my_mapreduce_map_memory_mb := 1024.
-Definition my_mapred_child_java_opts := mk_java_opts 200 M. 
-Definition my_mapreduce_map_java_opts := mk_java_opts 1024 M. 
-Definition my_mapreduce_reduce_java_opts := mk_java_opts 2560 M.
+Definition my_mapreduce_map_cpu_vcores := 4%Z.
+Definition my_mapreduce_map_memory_mb := 1024%Z.
+Definition my_mapred_child_java_opts := mk_java_opts 200%Z M. 
+Definition my_mapreduce_map_java_opts := mk_java_opts 200%Z M. 
+Definition my_mapreduce_reduce_java_opts := mk_java_opts 2560%Z M.
 Definition my_mapred_map_output_compression_type := "BLOCK".
 Definition my_mapreduce_ifile_readahead := true.
-Definition my_mapreduce_ifile_readahead_bytes := 4194%Z.
-Definition my_mapreduce_input_fileinputformat_split_maxsize := 2684%Z.
+Definition my_mapreduce_ifile_readahead_bytes := 4194304%Z.
+Definition my_mapreduce_input_fileinputformat_split_maxsize := 268435456%Z.
 Definition my_mapreduce_input_fileinputformat_split_minsize := 0%Z.
 Definition my_mapreduce_input_lineinputformat_linespermap := 1%Z.
 Definition my_mapreduce_job_counters_max := 120%Z.
@@ -33,15 +35,15 @@ Definition my_mapreduce_job_reduces := 1%Z.
 Definition my_mapreduce_job_running_map_limit := 0%Z.
 Definition my_mapreduce_job_running_reduce_limit := 0%Z.
 Definition my_mapreduce_job_speculative_minimum__allowed__tasks := 10%Z.
-Definition my_mapreduce_job_speculative_retry__after__no__speculate := 100%Z.
-Definition my_mapreduce_job_speculative_retry__after__speculate := 1500%Z.
+Definition my_mapreduce_job_speculative_retry__after__no__speculate := 1000%Z.
+Definition my_mapreduce_job_speculative_retry__after__speculate := 15000%Z.
 Definition my_mapreduce_job_speculative_speculative__cap__running__tasks := mk_float 0 1.
 Definition my_mapreduce_job_speculative_speculative__cap__total__tasks := mk_float 0 01.
-Definition my_mapreduce_job_split_metainfo_maxsize := 1000%Z.
+Definition my_mapreduce_job_split_metainfo_maxsize := 10000000%Z.
 Definition my_mapreduce_job_ubertask_enable := false.
 Definition my_mapreduce_job_ubertask_maxmaps := 9%Z.
 Definition my_mapreduce_job_ubertask_maxreduces := 1%Z.
-Definition my_mapreduce_jobtracker_expire_trackers_interval := 6000%Z.
+Definition my_mapreduce_jobtracker_expire_trackers_interval := 600000%Z.
 Definition my_mapreduce_jobtracker_handler_count := 10%Z.
 Definition my_mapreduce_jobtracker_maxtasks_perjob := 100%Z.
 Definition my_mapreduce_jobtracker_taskcache_levels := 2%Z.
@@ -65,21 +67,21 @@ Definition my_mapreduce_reduce_shuffle_input_buffer_percent := mk_float 0 7.
 Definition my_mapreduce_reduce_shuffle_memory_limit_percent := mk_float 0 25.
 Definition my_mapreduce_reduce_shuffle_merge_percent := mk_float 0 66.
 Definition my_mapreduce_reduce_shuffle_parallelcopies := 5%Z.
-Definition my_mapreduce_reduce_shuffle_retry__delay_max_ms := 6000%Z.
+Definition my_mapreduce_reduce_shuffle_retry__delay_max_ms := 60000%Z.
 Definition my_mapreduce_shuffle_max_connections := 0%Z.
 Definition my_mapreduce_shuffle_max_threads := 0%Z.
-Definition my_mapreduce_shuffle_transfer_buffer_size := 1310%Z.
-Definition my_mapreduce_task_combine_progress_records := 1000%Z.
+Definition my_mapreduce_shuffle_transfer_buffer_size := 131072%Z.
+Definition my_mapreduce_task_combine_progress_records := 10000%Z.
 Definition my_mapreduce_task_io_sort_factor := 10%Z.
 Definition my_mapreduce_task_io_sort_mb := 100%Z.
-Definition my_mapreduce_task_merge_progress_records := 1000%Z.
+Definition my_mapreduce_task_merge_progress_records := 10000%Z.
 Definition my_mapreduce_task_profile_maps := mk_range 0 2.
 Definition my_mapreduce_task_profile_reduces := mk_range 0 2.
 Definition my_mapreduce_tasktracker_http_threads := 40%Z.
 Definition my_apreduce_tasktracker_indexcache_mb := 10%Z.
 Definition my_mapreduce_tasktracker_map_tasks_maximum := 2%Z.
 Definition my_mapreduce_tasktracker_reduce_tasks_maximum := 2%Z.
-Definition my_mapreduce_tasktracker_taskmemorymanager_monitoringinterval := 500%Z.
+Definition my_mapreduce_tasktracker_taskmemorymanager_monitoringinterval := 5000%Z.
 
 (*
 Configuration proofs of constraint integrity.
@@ -100,7 +102,11 @@ Theorem my_mapreduce_map_java_opts_lt_my_mapreduce_map_memory_mb:
                   heap_size my_mapreduce_map_java_opts <= my_mapreduce_map_memory_mb.
 simpl. unfold my_mapreduce_map_memory_mb. omega. Qed.
 
-(*define a configuation for MapReduce sub-component*)
+(*
+Define a configuation for MapReduce sub-component.
+
+In Hadoop, this configuration should go into mapred-site.xml
+*)
 Definition my_mapred_config := mk_mapred_config total_cpu total_mem 
   my_mapred_child_java_opts        (* mapred_child_java_opts: JavaOpts *)
   my_mapred_map_output_compression_type       (* mapred_map_output_compression_type: string *)
@@ -176,7 +182,10 @@ Definition my_mapred_config := mk_mapred_config total_cpu total_mem
   my_mapreduce_map_memory_mb_le_total_mem
   my_mapreduce_map_java_opts_lt_my_mapreduce_map_memory_mb.
 
-(* define a configuation for HDFS sub-component *)
+(* define a configuation for HDFS sub-component.
+
+These parameters are in hdfs-site.xml
+*)
 Definition my_fs_namenode_handler_count := 10%Z.
 Definition my_dfs_replication := 3%Z.
 
@@ -185,14 +194,17 @@ Definition my_hdfs_config := mk_hdfs_config
   my_fs_namenode_handler_count                 (* dfs_namenode_handler_count: nat *)
   my_dfs_replication.                          (* dfs_replication: nat *)
 
-(*define a configuation for CORE sub-component*)
+(*define a configuation for CORE sub-component.
+
+These parameters are in core-site.xml
+*)
 Definition my_io_compression_codecs := "org.apache.hadoop.io.compress.DefaultCodec".
 Definition my_io_file_buffer_size := 4096%Z.
 Definition my_io_map_index_interval := 128%Z.
 Definition my_io_map_index_skip := 0%Z.
-Definition my_io_seqfile_compress_blocksize := 1000%Z.
-Definition my_io_seqfile_sorter_recordlimit := 1000%Z.
-Definition my_ipc_maximum_data_length := 6710%Z.
+Definition my_io_seqfile_compress_blocksize := 1000000%Z.
+Definition my_io_seqfile_sorter_recordlimit := 1000000%Z.
+Definition my_ipc_maximum_data_length := 67108864%Z.
 
 Definition my_core_config := mk_hadoop_core_config
   my_io_compression_codecs   			  (* io_compression_codecs: string *)
@@ -204,8 +216,12 @@ Definition my_core_config := mk_hadoop_core_config
   my_ipc_maximum_data_length.       (* ipc_maximum_data_length: nat *)
 
 
-(*define a configuation for CORE sub-component*)
-Definition my_yarn_app_mapreduce_am_command__opts := mk_java_opts 1024 M.
+(*
+Define a configuation for YARN sub-component.
+
+These parameters are in yarn-site.xml
+*)
+Definition my_yarn_app_mapreduce_am_command__opts := mk_java_opts 1024%Z M.
 Definition my_yarn_app_mapreduce_am_containerlauncher_threadpool__initial__size := 10%Z.
 Definition my_yarn_app_mapreduce_am_job_task_listener_thread__count := 30%Z.
 Definition my_yarn_app_mapreduce_am_resource_cpu__vcores := 1%Z.
@@ -213,8 +229,8 @@ Definition my_yarn_app_mapreduce_am_resource_mb := 1536%Z.
 Definition my_yarn_nodemanager_container__manager_thread__count := 20%Z.
 Definition my_yarn_nodemanager_localizer_client_thread__count := 5%Z.
 Definition my_yarn_nodemanager_localizer_fetch_thread__count := 4%Z.
-Definition my_yarn_nodemanager_resource_cpu__vcores := 0%Z.
-Definition my_yarn_nodemanager_resource_memory__mb := 0%Z.
+Definition my_yarn_nodemanager_resource_cpu__vcores := -1%Z.
+Definition my_yarn_nodemanager_resource_memory__mb := -1%Z.
 Definition my_yarn_nodemanager_resource_percentage__physical__cpu__limit := 100%Z. (*100 means 100%*)
 Definition my_yarn_nodemanager_vmem__check__enabled := true.
 Definition my_yarn_nodemanager_windows__container_cpu__limit_enabled := false.
@@ -227,7 +243,7 @@ Definition my_yarn_resourcemanager_scheduler_class := "org.apache.hadoop.yarn.se
 Definition my_yarn_resourcemanager_scheduler_client_thread__count := 50%Z.
 Definition my_yarn_resourcemanager_store_class := "org.apache.hadoop.yarn.server.resourcemanager.recovery.FileSystemRMStateStore".
 Definition my_yarn_scheduler_increment__allocation__mb := 512%Z.
-Definition my_yarn_scheduler_maximum__allocation__mb := 819%Z.
+Definition my_yarn_scheduler_maximum__allocation__mb := 8192%Z.
 Definition my_yarn_scheduler_maximum__allocation__vcores := 1%Z.
 Definition my_yarn_scheduler_minimum__allocation__mb := 1024%Z.
 Definition my_yarn_scheduler_minimum__allocation__vcores := 1%Z.
